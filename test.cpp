0a8552dd5959bca0d7ac7f928b7b13311930a9a3
Daniel Arnold <darnold@darnold.org>
2021-01-04 12:36:37 +0100

    Pigstuff

--- test.cpp
+++ test.cpp
@@ -1,7 +1,51 @@
 #include "gtest/gtest.h"
-#include "Fibonacci.hpp"
+#include "schwein.hpp"
+#include "gtest/gtest.h"
 
-TEST(test_my_class, test_10th_fibonacci_nr_is_55)
-{
-    EXPECT_EQ(Fibonacci::fibonacci(10), 55);
+using namespace testing;
+using namespace tiere;
+
+class pig_test : public Test {
+public:
+    pig_test() {}
+
+//    void SetUp(const std::string name) {
+//        objectUnderTest = schwein(name);
+//    }
+
+    schwein objectUnderTest;
+};
+
+TEST_F(pig_test, pig_start_weight_is_10) {
+    EXPECT_EQ(objectUnderTest.get_gewicht(), 10);
+}
+
+TEST_F(pig_test, pig_throws_if_constructed_as_elsa) {
+    EXPECT_THROW(
+            schwein objectUnderTest("Elsa"), std::invalid_argument);
+}
+
+TEST_F(pig_test, pig_likes_other_names) {
+    EXPECT_NO_THROW(schwein objectUnderTest("Horst"));
+}
+
+TEST_F(pig_test, pig_throws_if_setname_with_elsa) {
+    EXPECT_THROW(
+            objectUnderTest.set_name("Elsa"), std::invalid_argument);
+}
+
+TEST_F(pig_test, pig_likes_to_get_names_set_with_other_names) {
+    EXPECT_NO_THROW(objectUnderTest.set_name("Horst"));
+}
+
+TEST_F(pig_test, pig_is_fatter_after_eating) {
+    EXPECT_NO_THROW(objectUnderTest.fressen());
+    EXPECT_EQ(objectUnderTest.get_gewicht(), 11);
+}
+
+TEST_F(pig_test, pig_ostream_returns_the_name_and_weight) {
+    EXPECT_NO_THROW(objectUnderTest.set_name("Rüdiger"));
+    std::stringstream pig_stream;
+    pig_stream << objectUnderTest;
+    EXPECT_EQ(pig_stream.str(), "Schwein: Name=Rüdiger, Gewicht=10");
 }
