bb854536043140876bc1dce88fbd39533c61e8d6
Daniel Arnold <darnold@darnold.org>
2021-01-06 10:27:10 +0100

    Added qt test

---
+++ euro_to_dollar/test_main.cpp
@@ -0,0 +1,14 @@
+#include "euro_to_dollar_application_test.h"
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
